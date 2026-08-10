6cf1868a38bc966f90d41a1433e26f392ccd1619
picramag4-crypto <picramag4@gmail.com>
2026-08-10 21:06:05 +0300

    Update telegram.mjs

--- api/telegram.mjs
+++ api/telegram.mjs
@@ -34,7 +34,7 @@ export default {
 
       async function setButtons(inlineKeyboard) {
         await fetch(
-          https://api.telegram.org/bot${token}/editMessageReplyMarkup,
+          `https://api.telegram.org/bot${token}/editMessageReplyMarkup`,
           {
             method: "POST",
             headers: {
